e843d9263b6354b03ef20cb2e2bb672acdd6dac9
AdminBest1 <67699739+AdminBest1@users.noreply.github.com>
2021-03-31 13:17:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// // alert('Привет мир')
+
+
+
+// var btn = document.getElementById('btn')
+
+
+
+// btn.onclick = function () {
+// 	var text = document.querySelector('p.intro');
+// 	text.classList.add('green')
+// 	var img = document.querySelector('.student');
+// 	img.style.display = 'none';
+// 	document.querySelector('.skill-change').style.none.marginLeft = '50px'
+
+// 	}	
+// 	 $(window).scroll(function() {
+//         $('#learn .section-title').each(function(){
+//             var imagePos = $(this).offset().top;
+//             var topOfWindow = $(window).scrollTop();
+//             if (imagePos < topOfWindow+650) {
+//                 $(this).addClass("fadeInUp");
+//             }
+//         });
+//     });
+// 	 $(window).scroll(function() {
+//         $('.skill').each(function(){
+//             var imagePos = $(this).offset().top;
+//             var topOfWindow = $(window).scrollTop();
+//             if (imagePos < topOfWindow+650) {
+//                 $(this).addClass("fadeInUp");
+//             }
+//         });
+//     });
